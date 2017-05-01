d16c0fa11c01d2ee0436eb6003b05da6e9b5c344
Rodrigo Boniatti <boniatti.rodrigo@gmail.com>
2017-05-01 13:39:42 -0300

    Fix Sequelize bootstrap

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(module.filename)
 const DBconfig = config[process.env.NODE_ENV]
 let db = {}
 
-const sequelize = process.env.NODE_ENV
+const sequelize = process.env.NODE_ENV === 'development'
   ? new Sequelize(DBconfig.database, DBconfig.username, DBconfig.password, DBconfig)
   : new Sequelize(process.env[config.use_env_variable])
 
