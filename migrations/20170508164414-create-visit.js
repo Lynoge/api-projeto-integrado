72818af1fac215ceb915831605e20106ecb51d63
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2017-05-09 20:41:47 -0300

    Migration criar visita

---
+++ migrations/20170508164414-create-visit.js
@@ -0,0 +1,42 @@
+
+'use strict'
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('Visit', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      professionalId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Professional', key: 'id' }
+      },
+      requesterId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' }
+      },
+      canceled: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+      }
+    })
+  },
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Visit')
+  }
+}
