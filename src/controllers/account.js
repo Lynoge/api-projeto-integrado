c4ea308b3722878df6fbe4cc599c9e754bf403c4
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2017-07-02 12:07:07 -0300

    Salvar token

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,6 +1,4 @@
 import HttpStatus from 'http-status'
-// import jwt from 'jwt-simple'
-// import moment from 'moment'
 import ProfessionalRepository from '../infra/repository/professional'
 import RequesterRepository from '../infra/repository/requester'
 import accountValidation from '../helpers/accountValidation'
@@ -10,12 +8,8 @@ import sha1 from 'sha1'
 const professionalRepository = new ProfessionalRepository()
 const requesterRepository = new RequesterRepository()
 const secret = 'tokenSecret'
-const generateToken = (id) => {
-  return sha1(id)
-  // return jwt.encode({
-  //   iss: id,
-  //   exp: moment().add(7, 'days').valueOf()
-  // }, secret)
+const generateToken = () => {
+  return sha1(new Date() + Math.random().toFixed(3))
 }
 
 export default class Controller {
@@ -24,6 +18,7 @@ export default class Controller {
     const account = req.body
     try {
       accountValidation(account)
+      account.token = generateToken()
       let repository
       if (account.type === 'P')
         repository = professionalRepository
@@ -31,7 +26,6 @@ export default class Controller {
         repository = requesterRepository
       repository.create(account)
         .then(id => {
-          account.token = generateToken(id)
           res.status(HttpStatus.CREATED)
           res.json({ user: account })
         })
